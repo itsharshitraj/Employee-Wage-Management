059a4e89f071c6e261007680241c2be00d37cbd3
Harshit <itsharshitraj@gmail.com>
2025-03-01 15:12:01 +0530

    [Harshit]Add . UC-4: Calculated monthly wage for 20 working days

--- com/bridgelabz/src/utils/employeeUtils.js
+++ com/bridgelabz/src/utils/employeeUtils.js
@@ -5,16 +5,16 @@
     const PART_TIME_HOURS=4;
     const FULL_TIME_HOURS=8;
     const WAGE_PER_HOURS=20;
+    const WORKING_DAYS_IN_MONTH = 20;
 
     // work hours based on employee type
-    const getWorkHours= () => {
+    const getWorkHours = () => {
         let empCheck = Math.floor(Math.random()*10)%3;
      
         switch(empCheck){
             case PART_TIME:
             return PART_TIME_HOURS;
-            
-                
+              
             case FULL_TIME:
                 return FULL_TIME_HOURS;
                
@@ -27,8 +27,19 @@
 
     // Calculates daily wage using work hours.
     const calculateDailyWage = () => {
-        return getWorkHours() * WAGE_PER_HOURS;
+        let workHours=getWorkHours();
+        return workHours * WAGE_PER_HOURS;
     };
 
+    // Calculates monthly wage for 20 working days.
 
-    module.exports = { getWorkHours,calculateDailyWage};
+    const calculateMonthlyWage = () => {
+        let totalWage=0;
+        for(let day=1; day<=WORKING_DAYS_IN_MONTH;day++){
+            let dailyWage = calculateDailyWage();
+            totalWage += dailyWage;
+        }
+        return totalWage;
+    };
+
+    module.exports = { getWorkHours,calculateDailyWage,calculateMonthlyWage};
